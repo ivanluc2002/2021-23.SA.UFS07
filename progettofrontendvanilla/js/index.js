dbdbba3907487a491f243bd457895b36f8101699
Your Nameivanluc2002 <i.lucchini@itsrizzoli.it>
2022-05-11 16:38:18 +0200

    fix: test

--- progettofrontendvanilla/js/index.js
+++ progettofrontendvanilla/js/index.js
@@ -13,6 +13,7 @@ function backgroundChange(){
     var b="linear-gradient(90deg, #484c5c, #615872, #816382, #a56c8a, #c8778a, #e48682, #f89b76, #ffb56b);";
     var c="linear-gradient(90deg, #000b5c, #500863, #7f1265, #a72a63, #c74961, #e16b5f, #f38f62, #ffb56b);";
     var d=0;
+    console.log(d);
     d=Math.floor(Math.random() * 2)+1;
     if (d==1){
         document.getElementById("body").style.background=a;
