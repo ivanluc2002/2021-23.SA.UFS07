eda523dd093f356c1077d3d31af88d64a77bcbf5
Your Nameivanluc2002 <i.lucchini@itsrizzoli.it>
2022-05-31 10:47:58 +0200

    fix: update

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import {Button} from "luccoreact/atoms/button/Button";
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
+        <Button label={"gasssss"} />
     </div>
 );
 
