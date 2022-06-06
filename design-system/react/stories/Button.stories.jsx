e7e93673271510e80975688f76fd08c71e3aadb4
Your Nameivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-06 11:15:42 +0200

    feat: add button types

--- design-system/react/stories/Button.stories.jsx
+++ design-system/react/stories/Button.stories.jsx
@@ -15,26 +15,24 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const Reset = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+Reset.args = {
+  label: 'Button_reset',
+  type: 'reset',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Submit = Template.bind({});
+Submit.args = {
+  label: 'Button_submit',
+  type: 'submit' ,
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const Bottone = Template.bind({});
+Bottone.args = {
+  label: 'Button_bottone',
+  type: 'button' ,
+  
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+
