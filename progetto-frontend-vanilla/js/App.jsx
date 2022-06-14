d967f19647560cbc97d9e5b794a3fe116544f4cd
Your Nameivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-14 13:17:49 +0200

    feat:paragraphinapp

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {Button} from "luccoreact/atoms/button/Button";
 import {Input} from "luccoreact/atoms/input/Input";
-
+import {Paragraph} from "luccoreact/molecules/paragraph/Paragraph";
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
         <Button label={"gasssss"} />
         <Input label={"provaprova"}/>
+        <Paragraph testo={"zaooooooooo"}/>
     </div>
 );
 
