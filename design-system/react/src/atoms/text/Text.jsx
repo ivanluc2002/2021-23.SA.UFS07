e85b7a3027b427ec5521a07e9379fd3c5e29a03b
Your Nameivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-14 12:22:17 +0200

    feat : addtextandparagraph

---
+++ design-system/react/src/atoms/text/Text.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export const Text = ({ label, onClick, type }) => {
+  return (
+    <span
+     
+    >  
+    ciao
+    </span>
+  );
+};
+
+Text.propTypes = {
+  /**
+   * Button contents
+   */
+
+};
+
+Text.defaultProps = {
+  onClick: undefined,
+  type: 'text',
+  
+};
---
+++ design-system/react/stories/Text.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Text } from '../src/atoms/text/Text';
+export default {
+    title: 'Example/Atoms/Text',
+    component: Text,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+     
+    },
+  };
+  const Template = (args) => <Text {...args} />;
+
+export const Textprova = Template.bind({});
+Textprova.args = {
+  
+  
+  
+};
