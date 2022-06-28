45d451105a2428118204a246942a8ba50cd1715f
Your Nameivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-28 12:19:17 +0200

    feat: tailwind in button

--- design-system/react/src/atoms/button/Button.jsx
+++ design-system/react/src/atoms/button/Button.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 export const Button = ({ label, onClick, type }) => {
   return (
     <button
+      tw="hover:border-black"
       onClick={onClick}
       type={type}
     >
